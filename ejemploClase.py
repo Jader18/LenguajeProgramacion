74608768d61ae3c2619dce796ed972dcf9ba43dd
Jader18 <mendozajader18@gmail.com>
2024-02-10 08:48:19 -0600

    ejemplo de Clases, sesion 4

---
+++ ejemploClase.py
@@ -0,0 +1,23 @@
+#ejemplo de clases en python
+
+
+class Persona:
+    #se usan doble _ antes y despues de init para definir los parametros de la clase, es parte de la sintaxis
+    #self se usa siempre, para hacer referencia a las propiedades de la clase.  
+    def __init__(self, nombre, edad): 
+        self.nombre = nombre
+        self.edad = edad 
+
+    def saludar(self): 
+        #opcion 1 para imprimir
+        print(f"Hola, soy {self.nombre} y tengo {self.edad} años")
+
+        #opcion 2 para imprimir
+        print("Hola, soy ", self.nombre, " y tengo ", self.edad, "años")
+
+#crear un objeto persona
+persona1 = Persona("Duran", 79)
+persona1.saludar()
+
+persona2 = Persona("Jader", 21)
+persona2.saludar()
