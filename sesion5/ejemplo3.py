9b03d986a326676750bd3480a9701658a6f46083
Jader18 <mendozajader18@gmail.com>
2024-02-17 09:06:17 -0600

    Listas

--- sesion5/ejemplo3.py
+++ sesion5/ejemplo3.py
@@ -1,1 +1,17 @@
 #listas
+
+lista = [1, 2, 3, "manzana", 15.6, True]
+
+print(lista)
+
+
+#tamaño de la lista
+tam = len(lista)
+print("El tamaño de la lista es: ", tam)
+
+#Imprimir elemento de la lista
+print(lista[0])
+
+#imprimir ultimo elemento de la lista
+print(lista[tam-1])
+
